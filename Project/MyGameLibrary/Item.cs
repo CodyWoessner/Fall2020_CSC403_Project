a8c937eecb4fb61103940dd75a04d54dccf26b22
semiferozoe <45213836+semiferozoe@users.noreply.github.com>
2021-11-03 11:17:31 -0500

    Add files via upload
    push item.cs

---
+++ Project/MyGameLibrary/Item.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Fall2020_CSC403_Project.code {
+  public class Item {
+    public string ItemID;
+    public string ItemName;
+    public System.Drawing.Bitmap ItemImage;
+    public string ItemDesc;
+
+    public Item(string ItemID, string ItemName, System.Drawing.Bitmap ItemImage, string ItemDesc)
+    {
+      this.ItemID = ItemID;
+      this.ItemName = ItemName;
+      this.ItemImage = ItemImage;
+      this.ItemDesc = ItemDesc;
+    }
+
+  }
+  public class Weapon : Item
+  {
+    public int StrMod;
+    public Weapon(string ItemID, string ItemName, System.Drawing.Bitmap ItemImage, string ItemDesc) : base(ItemID, ItemName, ItemImage, ItemDesc)
+    {
+
+    } 
+  }
+
+  public class Consumable : Item
+  {
+    // private ? ConsAttribute;
+    public Consumable(string ItemID, string ItemName, System.Drawing.Bitmap ItemImage, string ItemDesc) : base(ItemID, ItemName, ItemImage, ItemDesc)
+    {
+
+    }
+  }
+
+  public class Armor : Item
+  {
+    // private ? ArmorAttribute;
+    public Armor(string ItemID, string ItemName, System.Drawing.Bitmap ItemImage, string ItemDesc) : base(ItemID, ItemName, ItemImage, ItemDesc)
+    {
+
+    }
+  }
+}
